a31e0d9890c36df37e8a2278835a0c20c7990859
João Vinicius Yuji Nakano <103541244+HauteHippie@users.noreply.github.com>
2022-04-26 21:07:34 -0300

    Update car.js

--- car.js
+++ car.js
@@ -1,15 +1,29 @@
 // Car variables
-let xCar = 645;
-let yCar = 42;
+let xCars = [645, 645, 645];
+let yCars = [43, 80, 150];
+let velocity = [2, 3, 4];
 let carDiameter = 66;
 
 function showCar() {
-  image(car, xCar, yCar, 66, 36)
+  for (let i = 0; i < cars.length; i++) {
+    image(cars[i], xCars[i], yCars[i], 56, 33)
+  }
 }
 
 function moveCar() {
-  xCar -= 5;
-  if (xCar + carDiameter < 0) {
-    xCar = 800;
+  for (let i = 0; i < cars.length; i++) {
+    xCars[i] -= velocity[i];
   }
+}
+
+function returnToPosition() {
+  for (let i = 0; i < cars.length; i++) {
+    if (verifyWallCollision(i)) {
+      xCars[i] = 800;
+    }
+  }
+}
+
+function verifyWallCollision(index) {
+  return xCars[index] + carDiameter < 0;
 }
