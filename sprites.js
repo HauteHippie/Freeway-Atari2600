3f3562e2530862530a609be69edde7bf2482926b
João Vinicius Yuji Nakano <103541244+HauteHippie@users.noreply.github.com>
2022-04-26 21:05:58 -0300

    Update sprites.js

--- sprites.js
+++ sprites.js
@@ -1,10 +1,13 @@
 // Imagens
 let road;
-let car;
+let car1, car2, car3;
 let chicken;
 
 function preload() {
   road = loadImage("sprites/background.png");
-  car = loadImage("sprites/car-2.png");
+  car1 = loadImage("sprites/car-1.png");
+  car2 = loadImage("sprites/car-2.png");
+  car3 = loadImage("sprites/car-3.png");
   chicken = loadImage("sprites/chicken.png");
+  cars = [car1, car2, car3];
 }
